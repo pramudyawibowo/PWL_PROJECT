4322a396621c8366c93e265a84aeafe3a2cbc532
Abdur097 <1941720094@student.polinema.ac.id>
2021-06-08 20:39:15 +0700

    Perbaikan CRUD teknisi

--- app/Http/Controllers/TeknisiController.php
+++ app/Http/Controllers/TeknisiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class TeknisiController extends Controller
 {
@@ -15,8 +16,8 @@ class TeknisiController extends Controller
      */
     public function index()
     {
-        $admin = User::where('level', 'teknisi')->get();
-        return view('teknisi.index', compact('admin'));
+        $teknisi = User::where('level', 'teknisi')->get();
+        return view('teknisi.index', compact('teknisi'));
     }
 
     /**
@@ -44,16 +45,23 @@ class TeknisiController extends Controller
             'level'     => 'required',
         ]);
 
-        $user = new User;
-        $user->name = $request->get('name');
-        $user->email = $request->get('email');
-        $user->password = bcrypt($request->get('password'));
-        $user->level = $request->get('level');
-        $user->remember_token = Str::random(60);
-        $user->save();
+        if($request->file('fotoprofil')){
+            $image_name = $request->file('fotoprofil')->store('images/user_profile', 'public');
+        } else {
+            $image_name = 'images/user_profile/user.png';
+        }
+
+        $teknisi = new User;
+        $teknisi->name = $request->get('name');
+        $teknisi->email = $request->get('email');
+        $teknisi->password = bcrypt($request->get('password'));
+        $teknisi->foto = $image_name;
+        $teknisi->level = $request->get('level');
+        $teknisi->remember_token = Str::random(60);
+        $teknisi->save();
 
         return redirect()->route('teknisi.index')
-            ->with('success', 'Admin Berhasil Ditambahkan');;
+            ->with('success', 'Teknisi Berhasil Ditambahkan');
     }
 
     /**
@@ -87,7 +95,32 @@ class TeknisiController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name'      => 'required',
+            'email'     => 'required',
+            'level'     => 'required',
+        ]);
+
+        $teknisi = User::find($id);
+
+        if($request->has('fotoprofil')){
+            if($teknisi->foto != 'images/user_profile/user.png' && file_exists(storage_path('app/public/'.$teknisi->foto))){
+                Storage::delete('public/'.$teknisi->foto);
+            }
+            $image_name = $request->file('fotoprofil')->store('images/user_profile', 'public');
+            $teknisi->foto = $image_name;
+        }
+
+        $teknisi->name = $request->get('name');
+        $teknisi->email = $request->get('email');
+        $teknisi->level = $request->get('level');
+        if($request->filled('password')){
+            $teknisi->password = bcrypt($request->get('password'));
+        }
+        $teknisi->save();
+
+        return redirect()->route('teknisi.index')
+            ->with('success', 'Teknisi berhasil diupdate');
     }
 
     /**
@@ -98,6 +131,8 @@ class TeknisiController extends Controller
      */
     public function destroy($id)
     {
-        //
+        User::find($id)->delete();
+        return redirect()->route('teknisi.index')
+            ->with('success', 'Teknisi berhasil dihapus');
     }
 }
