82c46fd25784dfe2fd38d6e5eb216f3cc82b4073
pramudyapolinema <1941720054@student.polinema.ac.id>
2021-06-09 16:42:14 +0700

    Membuat controller untuk tabel category & order

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kategori = Category::all();
+        return view('kategori.index', compact('kategori'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_kategori' => 'required',
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('kategori.index')
+            ->with('success', 'Kategori Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_kategori' => 'required',
+        ]);
+
+        $kategori = Category::find($id);
+        $kategori->nama_kategori = $request->get('nama_kategori');
+        $kategori->save();
+
+        return redirect()->route('kategori.index')
+            ->with('success', 'Kategori Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Category::find($id)->delete();
+        return redirect()->route('kategori.index')
+            ->with('success', 'Kategori Berhasil Dihapus');
+    }
+}
---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pesanan = Order::all();
+        $kategori = Category::all();
+        return view('pesanan.index', compact('pesanan', 'kategori'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_pesanan',
+            'nama_pelanggan',
+            'alamat_pelanggan',
+            'no_hp_pelanggan',
+            'nama_barang',
+            'id_kategori',
+            'keluhan',
+        ]);
+
+        Order::create($request->all());
+
+        return redirect()->route('pesanan.index')
+            ->with('success', 'Data pesanan berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_pesanan',
+            'nama_pelanggan',
+            'alamat_pelanggan',
+            'no_hp_pelanggan',
+            'nama_barang',
+            'id_kategori',
+            'keluhan',
+        ]);
+
+        $pesanan = Order::find($id);
+        $pesanan->nama_pesanan = $request->get('nama_pesanan');
+        $pesanan->nama_pelanggan = $request->get('nama_pelanggan');
+        $pesanan->alamat_pelanggan = $request->get('alamat_pelanggan');
+        $pesanan->no_hp_pelanggan = $request->get('no_hp_pelanggan');
+        $pesanan->nama_barang = $request->get('nama_barang');
+        $pesanan->id_kategori = $request->get('id_kategori');
+        $pesanan->keluhan = $request->get('keluhan');
+        $pesanan->save();
+
+        return redirect()->route('pesanan.index')
+            ->with('success', 'Pesanan berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Order::find($id)->delete();
+        return redirect()->route('pesanan.index')
+            ->with('success', 'Pesanan berhasil dihapus');
+    }
+}
